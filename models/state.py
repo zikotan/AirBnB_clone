564acdf93823a3d7cdca64bcc87c63ab6a616df1
zikotan <tananzakaria630@gmail.com>
2024-01-11 18:14:07 -0800

    response

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""the State class"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """State
+    
+    Attributes:
+        name (str): arg
+    """
+    
+    name = ""
--- models/user.py
+++ models/user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""the user class"""
+"""the User class"""
 from models.base_model import BaseModel
 
 

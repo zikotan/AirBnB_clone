b48c908b984619f95ac6f019135ac0a0572cace4
zikotan <tananzakaria630@gmail.com>
2024-01-11 18:43:49 -0800

    response

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""the Amenity class"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Amenity
+    
+    Attributes:
+        name (str): arg1
+    """
+    
+    name = ""

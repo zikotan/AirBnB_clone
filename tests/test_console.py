580c805b356c3b89a58eb9449113d4ec18121ca4
zikotan <tananzakaria630@gmail.com>
2024-01-13 07:20:21 -0800

    response

--- tests/test_console.py
+++ tests/test_console.py
@@ -27,6 +27,11 @@ class TestHBNBCommand_prompting(unittest.TestCase):
     def test_prompt_string(self):
         self.assertEqual("(hbnb) ", HBNBCommand.prompt)
 
+    def test_empty_line(self):
+        with patch('sys.stdout', new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd(""))
+            self.assertEqual("", output.getvalue().strip())
+
 
 if __name__ == "__main__":
     unittest.main()

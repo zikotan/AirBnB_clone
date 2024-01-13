38b32615cab8126d998903d2637b03c00f652f60
zikotan <tananzakaria630@gmail.com>
2024-01-13 09:30:08 -0800

    response

--- tests/test_console.py
+++ tests/test_console.py
@@ -24,9 +24,6 @@ from unittest.mock import patch
 class TestHBNBCommand_prompting(unittest.TestCase):
     """Unittests for testing prompting of the HBNB command interpreter."""
 
-    def test_prompt_string(self):
-        self.assertEqual("(hbnb) ", HBNBCommand.prompt)
-
     def test_empty_line(self):
         with patch("sys.stdout", new=StringIO()) as output:
             self.assertFalse(HBNBCommand().onecmd(""))

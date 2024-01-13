a4790dbc8536e55f3ecfc2b1313f6932ebe160dd
zikotan <tananzakaria630@gmail.com>
2024-01-13 07:45:09 -0800

    response

--- tests/test_console.py
+++ tests/test_console.py
@@ -42,6 +42,13 @@ class TestHBNBCommand_help(unittest.TestCase):
             self.assertFalse(HBNBCommand().onecmd("help quit"))
             self.assertEqual(c, f.getvalue().strip())
 
+    def test_help_create(self):
+        c = ("Use: create <class>\n        "
+        "Create a new class instance and print its id.")
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("help create"))
+            self.assertEqual(c, f.getvalue().strip())
+
 
 if __name__ == "__main__":
     unittest.main()

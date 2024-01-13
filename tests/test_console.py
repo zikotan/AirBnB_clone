3dace688ecea0c65269f3ed6be1821f26f7c31dc
zikotan <tananzakaria630@gmail.com>
2024-01-13 10:59:19 -0800

    response

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""Defines unittests for console.py.
+"""The console.py unittests.
 
-Unittest classes:
-    TestHBNBCommand_prompting
-    TestHBNBCommand_help
-    TestHBNBCommand_exit
-    TestHBNBCommand_create
-    TestHBNBCommand_show
-    TestHBNBCommand_all
-    TestHBNBCommand_destroy
-    TestHBNBCommand_update
+Unittets classes:
+     TestHBNBCommand_prompting
+     TestHBNBCommand_help
+     TestHBNBCommand_exit
+     TestHBNBCommand_create
+     TestHBNBCommand_show
+     TestHBNBCommand_all
+     TestHBNBCommand_destroy
+     TestHBNBCommand_update
 """
 import os
 import sys

2ce478ca2292656dfa338aad1daa8484677426b8
zikotan <tananzakaria630@gmail.com>
2024-01-13 08:16:30 -0800

    response

--- tests/test_console.py
+++ tests/test_console.py
@@ -84,6 +84,15 @@ class TestHBNBCommand_help(unittest.TestCase):
             self.assertFalse(HBNBCommand().onecmd("help count"))
             self.assertEqual(c, f.getvalue().strip())
 
+    def test_help_update(self):
+        c = ("Use: update <class> <id> <attribute_name> <attribute_value>\n        "
+        "or <class>.update(<id>, <attribute_name>, <attribute_value>)\n        "
+        "or <class>.update(<id>, <dictionary>)\n        "
+        "Updates a class instance of an id given.")
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("help update"))
+            self.assertEqual(c, f.getvalue().strip())
+
 
 if __name__ == "__main__":
     unittest.main()

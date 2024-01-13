69ae7d65a3a218145fbfc4861f917ede5d901385
zikotan <tananzakaria630@gmail.com>
2024-01-13 06:53:50 -0800

    response

---
+++ tests/test_console.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""The console.py unittests.
+
+Unittets classes:
+     TestHBNBCommand_prompting
+     TestHBNBCommand_help
+     TestHBNBCommand_exit
+     TestHBNBCommand_create
+     TestHBNBCommand_show
+     TestHBNBCommand_all
+     TestHBNBCommand_destroy
+     TestHBNBCommand_update
+"""
+import os
+import sys
+import unittest
+from models import storage
+from models.engine.file_storage import FileStorage
+from console import HBNBCommand
+from io import StringIO
+from unittest.mock import patch
+
+
+if __name__ == "__main__":
+    unittest.main()

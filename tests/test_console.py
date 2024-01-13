48f8db07aa27924ec6181b0a90175d717b612da1
zikotan <tananzakaria630@gmail.com>
2024-01-13 11:11:43 -0800

    response

--- tests/test_console.py
+++ tests/test_console.py
@@ -21,13 +21,39 @@ from io import StringIO
 from unittest.mock import patch
 
 
+class TestHBNBCommand_prompting(unittest.TestCase):
+    """The prompting unittests"""
+
+    def test_prompt_string(self):
+        self.assertEqual("(hbnb) ", HBNBCommand.prompt)
+
+    def test_empty_line(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd(""))
+            self.assertEqual("", f.getvalue().strip())
+
+
 class TestHBNBCommand_help(unittest.TestCase):
-    """Unittests for testing help messages of the HBNB command interpreter."""
+    """The help unittests"""
+
+    def test_help(self):
+        c = ("Documented commands (type help <topic>):\n"
+        "========================================\n"
+        "EOF  all  count  create  destroy  help  quit  show  update")
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("help"))
+            self.assertEqual(c, f.getvalue().strip())
 
     def test_help_quit(self):
         c = "Quit command to exit the program."
         with patch('sys.stdout', new=StringIO()) as f:
             self.assertFalse(HBNBCommand().onecmd("help quit"))
+            self.assertEqual(c, f.getvalue().strip())
+
+    def test_help_EOF(self):
+        c = "EOF signal to exit the program."
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("help EOF"))
             self.assertEqual(c, f.getvalue().strip())
 
     def test_help_create(self):
@@ -36,13 +62,7 @@ class TestHBNBCommand_help(unittest.TestCase):
         with patch('sys.stdout', new=StringIO()) as f:
             self.assertFalse(HBNBCommand().onecmd("help create"))
             self.assertEqual(c, f.getvalue().strip())
-
-    def test_help_EOF(self):
-        c = "EOF signal to exit the program."
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd("help EOF"))
-            self.assertEqual(c, output.getvalue().strip())
-
+    
     def test_help_show(self):
         c = ("Use: show <class> <id> or <class>.show(<id>)\n        "
         "Shows the representation of a class instance of an id given.")
@@ -64,7 +84,7 @@ class TestHBNBCommand_help(unittest.TestCase):
         with patch('sys.stdout', new=StringIO()) as f:
             self.assertFalse(HBNBCommand().onecmd("help all"))
             self.assertEqual(c, f.getvalue().strip())
-
+    
     def test_help_count(self):
         c = ("Use: count <class> or <class>.count()\n        "
         "Pritns the number of instances of the given class.")
@@ -79,6 +99,79 @@ class TestHBNBCommand_help(unittest.TestCase):
         "Updates a class instance of an id given.")
         with patch('sys.stdout', new=StringIO()) as f:
             self.assertFalse(HBNBCommand().onecmd("help update"))
+            self.assertEqual(c, f.getvalue().strip())
+
+
+class TestHBNBCommand_exit(unittest.TestCase):
+    """The exiting unittests"""
+
+    def test_qui_exits(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertTrue(HBNBCommand().onecmd("quit"))
+
+    def test_EOF_exits(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertTrue(HBNBCommand().onecmd("EOF"))
+
+
+class TestHBNBCommand_create(unittest.TestCase):
+    """The create unittests"""
+
+    @classmethod
+    def setUP(self):
+        try:
+            os.rename("file.json", "tmp")
+        except IOError:
+            pass
+        FileStorage.__objects = {}
+
+    @classmethod
+    def tearDown(self):
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+        try:
+            os.rename("tmp", "file.json")
+        except IOError:
+            pass
+
+    def test_create_missing_class(self):
+        c = "** class name missing **"
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("create"))
+            self.assertEqual(c, f.getvalue().strip())
+
+
+class TestHBNBCommand_show(unittest.TestCase):
+    """The show unittests"""
+
+    @classmethod
+    def setUP(self):
+        try:
+            os.rename("file.json", "tmp")
+        except IOError:
+            pass
+        FileStorage.__objects = {}
+
+    @classmethod
+    def tearDown(self):
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+        try:
+            os.rename("tmp", "file.json")
+        except IOError:
+            pass
+
+    def test_create_missing_class(self):        
+        c = "** class name missing **"
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("show"))
+            self.assertEqual(c, f.getvalue().strip())
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd(".show()"))
             self.assertEqual(c, f.getvalue().strip())
 
 

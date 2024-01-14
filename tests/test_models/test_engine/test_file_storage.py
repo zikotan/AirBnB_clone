43a940b42012b3a96411eed6cb40af21198edd2d
zikotan <tananzakaria630@gmail.com>
2024-01-14 11:47:28 -0800

    response

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -12,16 +12,14 @@ from models.base_model import BaseModel
 
 
 class TestBaseModel_methods(unittest.TestCase):
-    """The save method unittests"""
-
-    def test_all(self):
-        self.assertEqual(dict, type(models.storage.all()))
+    """The file_storage.py methods unittests"""
     
     def test_new(self):
         b = BaseModel()
         models.storage.new(b)
         self.assertIn("BaseModel." + b.id, models.all().keys())
         self.assertIn(b, models.all().values())
+
 
 if __name__ == "__main__":
     unittest.main()

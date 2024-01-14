dcf0f8cb6dbe00628d1d0043b8441e4f80181e29
zikotan <tananzakaria630@gmail.com>
2024-01-14 14:24:55 -0800

    response

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -41,15 +41,5 @@ class TestBaseModel_save(unittest.TestCase):
             self.assertIn(uId, f.read())
 
 
-class TestBaseModel_to_dict(unittest.TestCase):
-    """The to_dict method unittests"""
-    
-    def test_to_dict_date_attr_str(self):
-        c = BaseModel()
-        c_dict = c.to_dict()
-        self.assertEqual(str, type(c_dict["created_at"]))
-        self.assertEqual(str, type(c_dict["updated_at"]))
-
-
 if __name__ == "__main__":
     unittest.main()

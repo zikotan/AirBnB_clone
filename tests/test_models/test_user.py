c84595d3bcec2092a2bade4771c13076f3c3b99d
zikotan <tananzakaria630@gmail.com>
2024-01-14 14:59:30 -0800

    response

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -9,6 +9,32 @@ import unittest
 import os
 from models.user import User
 
+
+class TestBaseModel_instances(unittest.TestCase):
+    """The file_storage.py instansiation unittests"""
+
+    @classmethod
+    def setUp(self):
+        try:
+            os.rename("file.json", "tmp")
+        except IOError:
+            pass
+
+    @classmethod
+    def tearDown(self):
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+        try:
+            os.rename("tmp", "file.json")
+        except IOError:
+            pass
+
+    def test_inst_no_args(self):
+        self.assertEqual(type(User()), User)
+
+
 class TestBaseModel_save(unittest.TestCase):
     """The save method unittests"""
 

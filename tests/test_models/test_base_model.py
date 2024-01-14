585612f2b0b2c27bffefd7ab166b8b5e2a3c2d78
zikotan <tananzakaria630@gmail.com>
2024-01-14 08:51:27 -0800

    response

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,6 +2,8 @@
 """The base_model.py unittests
 Unittest classes:
     TestBaseModel_instance
+    TestBaseModel_save
+    TestBaseModel_to_dict
 """
 
 import os
@@ -45,6 +47,16 @@ class TestBaseModel_save(unittest.TestCase):
         cId = "BaseModel." + c.id
         with open("file.json", "r") as f:
             self.assertIn(cId, f.read())
+
+
+class TestBaseModel_to_dict(unittest.TestCase):
+    """The save method unittests"""
+
+    def test_to_dict_date_attr_str(self):
+        c = BaseModel()
+        c_dict = c.to_dict()
+        self.assertEqual(str, type(c_dict["created_at"]))
+        self.assertEqual(str, type(c_dict["updated_at"]))
 
 
 if __name__ == "__main__":

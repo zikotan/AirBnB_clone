c3f0a1cf95cc0d724581365cabc57b5cb912fc6c
zikotan <tananzakaria630@gmail.com>
2024-01-14 11:29:33 -0800

    response

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""The base_model.py unittests
+"""The file_storage.py unittests
 
 Unittest classes:
-    TestBaseModel_save
-    TestBaseModel_to_dict
+    TestBaseModel_methods
 """
 
 import models
@@ -12,20 +11,12 @@ from datetime import datetime
 from models.base_model import BaseModel
 
 
-class TestBaseModel_save(unittest.TestCase):
+class TestBaseModel_methods(unittest.TestCase):
     """The save method unittests"""
 
-    def test_save_update_file(self):
-        c = BaseModel()
-        c.save()
-        cId = "BaseModel." + c.id
-        with open("file.json", "r") as f:
-            self.assertIn(cId, f.read())
+    def test_all(self):
+        self.assertEqual(dict, type(models.storage.all()))
 
-
-class TestBaseModel_to_dict(unittest.TestCase):
-    """The to_dict method unittests"""
-    
     def test_to_dict_date_attr_str(self):
         c = BaseModel()
         c_dict = c.to_dict()

6ea5557340889b5c4bdd6668f41740f531f483f7
zikotan <tananzakaria630@gmail.com>
2024-01-14 14:22:54 -0800

    response

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -14,13 +14,31 @@ from models.base_model import BaseModel
 
 class TestBaseModel_save(unittest.TestCase):
     """The save method unittests"""
+    
+    @classmethod
+    def setUp(self):
+        try:
+            os.rename("file.json", "tmp")
+        except IOError:
+            pass
+
+    @classmethod
+    def tearDown(self):
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+        try:
+            os.rename("tmp", "file.json")
+        except IOError:
+            pass
 
     def test_save_update_file(self):
-        c = BaseModel()
-        c.save()
-        cId = "BaseModel." + c.id
+        u = User()
+        u.save()
+        uId = "User." + u.id
         with open("file.json", "r") as f:
-            self.assertIn(cId, f.read())
+            self.assertIn(uId, f.read())
 
 
 class TestBaseModel_to_dict(unittest.TestCase):

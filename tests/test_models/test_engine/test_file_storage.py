5a68bd8cf599473af8e8c8631b19815874a030b3
zikotan <tananzakaria630@gmail.com>
2024-01-14 12:18:27 -0800

    response

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -20,7 +20,14 @@ class TestBaseModel_methods(unittest.TestCase):
         self.assertIn("BaseModel." + b.id, models.storage.all().keys())
         self.assertIn(b, models.storage.all().values())
 
-    
+    def test_save(self):
+        u = BaseModel()
+        models.storage.new(u)
+        models.storage.save()
+        text = ""
+        with open("file.json", "r") as f:
+            text = f.read()
+            self.assertIn("BaseModel." + u.id, text)
 
 
 if __name__ == "__main__":

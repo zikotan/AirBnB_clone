5ea0cba6c6de2f24ee93012950d86544cdef17fc
zikotan <tananzakaria630@gmail.com>
2024-01-14 16:20:18 -0800

    response

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -3,67 +3,35 @@
 
 Unittest classes:
     TestBaseModel_save
+    TestBaseModel_to_dict
 """
 
+import models
 import unittest
-import os
-from models.user import User
-from datetime import datetime
-
-
-class TestBaseModel_instances(unittest.TestCase):
-    """The file_storage.py instansiation unittests"""
-
-    @classmethod
-    def setUp(self):
-        try:
-            os.rename("file.json", "tmp")
-        except IOError:
-            pass
-
-    @classmethod
-    def tearDown(self):
-        try:
-            os.remove("file.json")
-        except IOError:
-            pass
-        try:
-            os.rename("tmp", "file.json")
-        except IOError:
-            pass
-
-    def test_inst_no_args(self):
-        self.assertEqual(type(User()), User)
-
-    def test_inst_with_kwargs(self):
-        d = datetime.today()
-        d_form = d.isoformat()
-        u = User(id="0", created_at=d_form, updated_at=d_form)
-        self.assertEqual(u.id,  "0")
-        self.assertEqual(u.created_at, d)
-        self.assertEqual(u.updated_at, d)
+from datetime import datetime 
+from models.base_model import BaseModel
 
 
 class TestBaseModel_save(unittest.TestCase):
     """The save method unittests"""
 
     def test_save_update_file(self):
-        u = User()
-        u.save()
-        uId = "User." + u.id
+        c = BaseModel()
+        c.save()
+        cId = "BaseModel." + c.id
         with open("file.json", "r") as f:
-            self.assertIn(uId, f.read())
+            self.assertIn(cId, f.read())
 
 
 class TestBaseModel_to_dict(unittest.TestCase):
-    """The save method unittests"""
-
+    """The to_dict method unittests"""
+    
     def test_to_dict_date_attr_str(self):
-        u = User()
-        u_dict = u.to_dict()
-        self.assertEqual(str, type(u_dict["id"]))
-        self.assertEqual(str, type(u_dict["created_at"]))
-        self.assertEqual(str, type(u_dict["updated_at"]))
+        c = BaseModel()
+        c_dict = c.to_dict()
+        self.assertEqual(str, type(c_dict["created_at"]))
+        self.assertEqual(str, type(c_dict["updated_at"]))
+
 
 if __name__ == "__main__":
     unittest.main()
